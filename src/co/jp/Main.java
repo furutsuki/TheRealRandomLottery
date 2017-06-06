40cb839fdbb3414852c3ed1740146f76036064c8
furut <furut@192.168.2.105>
2017-06-06 23:03:36 +0900

    update

--- src/co/jp/Main.java
+++ src/co/jp/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("let me hear your voice");
+		CakesGenerater gc = new CakesGenerater();
+		gc.generateCakes(5, 40, 7);
 	}
 
 }
