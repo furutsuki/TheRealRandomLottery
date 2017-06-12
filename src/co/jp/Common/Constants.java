11302a6caa49a83663a92c0b8f5f7461e1ccc97e
HHHHuuu <furutsukimoxi@gmail.com>
2017-06-12 20:12:48 +0900

    for memory out error

---
+++ src/co/jp/Common/Constants.java
@@ -0,0 +1,7 @@
+package co.jp.Common;
+
+public class Constants {
+	public static final int LIMIT_COUNT = 1000000;
+	public static final int MSG_COUNT = 1000000;
+}
+// CodeCheck  ver1.1.10: 7f5fd1829086887ac4bca312c022d4fd2dcef228cf5f054d4fedbe897bd0cbe9
